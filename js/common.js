109f232a3c69a2a83fc7b9ee772f38a72419f6f3
akella <akella.a@gmail.com>
2014-06-11 01:20:32 +0300

    re

--- js/common.js
+++ js/common.js
@@ -1,44 +1,49 @@
 head.ready(function() {
 
 
-console.log('111');
-$.getJSON( "http://coderiver.github.io/borjomi/test.json", function( data ) {
-  console.log(data);
-  console.log('----------');
-   $.each( data, function( key, val ) {
-    console.log( "<li id='" + key + "'>" + val + "</li>" );
-  });
+
+$.getJSON( "http://coderiver.github.io/borjomi/test.js" )
+  .done(function( json ) {
+    g = json;
+    function createg(arr, targetdiv,title){
+		if(arr.length>0){
+			preresult = '<div class="hashtag"><div class="hashtag__title"><span>#'+title+'</span></div><div id="hastag__feed">';
+			postresult = '</div></div>'
+			result = '';
+			for (var i = 0; i < arr.length; i++) { 
+		    	result = result + "<a class='fancybox' href='"+arr[i].big+"'><img src='"+arr[i].small+"' alt='' /></a>"
+			}
+			$('#'+targetdiv).html(preresult+result+postresult);
+
+		}
+	}
+
+	a1 = g.firstgallery;
+	createg(a1,'firstgallery','borjomibus');
+
+
+
+  })
+  .fail(function( jqxhr, textStatus, error ) {
+    var err = textStatus + ", " + error;
+    console.log( "Request Failed: " + err );
 });
 
-g = { 
-"firstgallery":[
-    {"small":"img/poster1.jpg", "big":"img/01.jpg"},
-    {"small":"img/poster2.jpg", "big":"img/01.jpg"},
-    {"small":"img/poster3.jpg", "big":"img/01.jpg"}
-],
-"secondgallery":[
-    {"small":"img/poster4.jpg", "big":"img/01.jpg"},
-    {"small":"img/poster5.jpg", "big":"img/01.jpg"},
-    {"small":"img/poster6.jpg", "big":"img/01.jpg"}
-]
-};
+// g = { 
+// "firstgallery":[
+//     {"small":"img/poster1.jpg", "big":"img/01.jpg"},
+//     {"small":"img/poster2.jpg", "big":"img/01.jpg"},
+//     {"small":"img/poster3.jpg", "big":"img/01.jpg"}
+// ],
+// "secondgallery":[
+//     {"small":"img/poster4.jpg", "big":"img/01.jpg"},
+//     {"small":"img/poster5.jpg", "big":"img/01.jpg"},
+//     {"small":"img/poster6.jpg", "big":"img/01.jpg"}
+// ]
+// };
 
 
-function createg(arr, targetdiv,title){
-	if(arr.length>0){
-		preresult = '<div class="hashtag"><div class="hashtag__title"><span>#'+title+'</span></div><div id="hastag__feed">';
-		postresult = '</div></div>'
-		result = '';
-		for (var i = 0; i < arr.length; i++) { 
-	    	result = result + "<a class='fancybox' href='"+arr[i].big+"'><img src='"+arr[i].small+"' alt='' /></a>"
-		}
-		$('#'+targetdiv).html(preresult+result+postresult);
 
-	}
-}
-
-a1 = g.firstgallery;
-createg(a1,'firstgallery','borjomibus');
 
 
 
