4c16cb215a2d0ed7144fb6156f9627ef860e2b9e
akella <akella.a@gmail.com>
2014-06-11 01:34:07 +0300

    margins

--- test.js
+++ test.js
@@ -1,32 +1,31 @@
 { 
 "firstgallery":[
-    {"small":"img/poster1.jpg", "big":"img/01.jpg"},
-    {"small":"img/poster2.jpg", "big":"img/01.jpg"},
-    {"small":"img/poster3.jpg", "big":"img/01.jpg"}
+    {"small":"img/g.jpg", "big":"img/01.jpg"},
+    {"small":"img/g.jpg", "big":"img/01.jpg"},
+    {"small":"img/g.jpg", "big":"img/01.jpg"}
 ],
 "secondgallery":[
-    {"small":"img/poster4.jpg", "big":"img/01.jpg"},
-    {"small":"img/poster5.jpg", "big":"img/01.jpg"},
-    {"small":"img/poster6.jpg", "big":"img/01.jpg"}
+    {"small":"img/g.jpg", "big":"img/01.jpg"},
+    {"small":"img/g.jpg", "big":"img/01.jpg"},
+    {"small":"img/g.jpg", "big":"img/01.jpg"}
 ],
 "thirdgallery":[
-    {"small":"img/poster4.jpg", "big":"img/01.jpg"},
-    {"small":"img/poster5.jpg", "big":"img/01.jpg"},
-    {"small":"img/poster4.jpg", "big":"img/01.jpg"},
-    {"small":"img/poster5.jpg", "big":"img/01.jpg"},
-    {"small":"img/poster4.jpg", "big":"img/01.jpg"},
-    {"small":"img/poster5.jpg", "big":"img/01.jpg"},
-    {"small":"img/poster6.jpg", "big":"img/01.jpg"}
+    {"small":"img/g.jpg", "big":"img/01.jpg"},
+    {"small":"img/g.jpg", "big":"img/01.jpg"},
+    {"small":"img/g.jpg", "big":"img/01.jpg"},
+    {"small":"img/g.jpg", "big":"img/01.jpg"},
+    {"small":"img/g.jpg", "big":"img/01.jpg"},
+    {"small":"img/g.jpg", "big":"img/01.jpg"},
+    {"small":"img/g.jpg", "big":"img/01.jpg"}
 ],
 "fourthgallery":[
-    {"small":"img/poster4.jpg", "big":"img/01.jpg"},
-    {"small":"img/poster4.jpg", "big":"img/01.jpg"},
-    {"small":"img/poster5.jpg", "big":"img/01.jpg"},
-    {"small":"img/poster4.jpg", "big":"img/01.jpg"},
-    {"small":"img/poster5.jpg", "big":"img/01.jpg"},
-    {"small":"img/poster4.jpg", "big":"img/01.jpg"},
-    {"small":"img/poster5.jpg", "big":"img/01.jpg"},
-    {"small":"img/poster5.jpg", "big":"img/01.jpg"},
-    {"small":"img/poster6.jpg", "big":"img/01.jpg"}
+    {"small":"img/g.jpg", "big":"img/01.jpg"},
+    {"small":"img/g.jpg", "big":"img/01.jpg"},
+    {"small":"img/g.jpg", "big":"img/01.jpg"},
+    {"small":"img/g.jpg", "big":"img/01.jpg"},
+    {"small":"img/g.jpg", "big":"img/01.jpg"},
+    {"small":"img/g.jpg", "big":"img/01.jpg"},
+    {"small":"img/g.jpg", "big":"img/01.jpg"}
+ 
 ]
 }
